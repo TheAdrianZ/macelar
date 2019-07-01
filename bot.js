69ed8bc77ae820dd86a83c45d148fc85c2ebe2a9
TheAdrianZ <52346365+TheAdrianZ@users.noreply.github.com>
2019-07-01 21:04:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -121,6 +121,65 @@ client.on('message', message => {
   
   
   
+                                      client.on("message", (message) => {
+ 
+                                        if (message.content.startsWith("+new")) {  
+                                             const reason = message.content.split(" ").slice(1).join(" ");  
+                                             if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`Make A Role Named : Support Team | and give the bot **__ADMINISTRATOR__** permission `);
+                                             if (message.guild.channels.exists("name", "ticket-{message.author.id}" + message.author.id)) return message.channel.send(`You Already Have An Opened Ticket`);
+                                             message.guild.createChannel(`ticket-${message.author.username}`, "text").then(c => {
+                                                 let role = message.guild.roles.find("name", "Support Team");
+                                                 let role2 = message.guild.roles.find("name", "@everyone");
+                                                 c.overwritePermissions(role, {
+                                                     SEND_MESSAGES: true,
+                                                     READ_MESSAGES: true
+                                                 });  
+                                                 c.overwritePermissions(role2, {
+                                                     SEND_MESSAGES: false,
+                                                     READ_MESSAGES: false
+                                                 });
+                                                 c.overwritePermissions(message.author, {
+                                                     SEND_MESSAGES: true,
+                                                     READ_MESSAGES: true
+                                                 });
+                                                 message.channel.send(`Created Ticket #${c.name} :white_check_mark:`);
+                                                 const embed = new Discord.RichEmbed()
+                                                     .setColor(0xCF40FA)
+                                                     .addField(`Hey ${message.author.username}!`, `Your Ticket Is Created #ticket :white_check_mark:`)
+                                                     .setTimestamp();
+                                                 c.send({
+                                                     embed: embed
+                                                 });
+                                             }).catch(console.error);
+                                         }
+                                      
+                                      
+                                       if (message.content.startsWith("+close")) {
+                                             if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+                                      
+                                           message.channel.send(`Are You Sure You Want To Delete Your Ticket ?Write : **__+Confirm__* To Confirm Your Request`)
+                                               .then((m) => {
+                                                   message.channel.awaitMessages(response => response.content === '+Confirm', {
+                                                           max: 1,
+                                                           time: 10000,
+                                                           errors: ['time'],
+                                                       })  
+                                                       .then((collected) => {
+                                                           message.channel.delete();
+                                                       })  
+                                                       .catch(() => {
+                                                           m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+                                                               m2.delete();
+                                                           }, 3000);
+                                                       });
+                                               });
+                                       }
+                                      
+                                     });
+
+
+
+
         client.on('message', message => {
             if (message.content.startsWith("+id")) {
                          if(!message.channel.guild) return message.reply('** This Command Works Only In Servers **');
